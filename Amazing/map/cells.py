cff809f7f4856272c9fa19a46ae01cdfabf42a8d
Hugo Lherm <hugolher@etudiant.univ-reims.fr>
2023-03-13 19:22:42 +0100

    Création de la methode cells.create

--- Amazing/map/cells.py
+++ Amazing/map/cells.py
@@ -11,19 +11,32 @@ class Cells:
         self.width = width"""
         self.nb_cells = nb_cells
 
+
     def create(self):
         pair = 2
-        dict = []
-        for i in range(0, self.surface.get_height() + 1, self.surface.get_height() // self.nb_cells):
-            for j in range(0, self.surface.get_height() + 1, self.surface.get_height() // self.nb_cells):
+        self.cells = {}
+        ci = 0
+        test = []
+        for i in range(0, self.surface.get_height(), self.surface.get_height() // self.nb_cells):
+            cj = 0
+            for j in range(0, self.surface.get_height(), self.surface.get_height() // self.nb_cells):
                 pygame.display.flip()
-                if pair % 2 == 0:
-                    pygame.Surface((self.surface.get_height(),
-                                    self.surface.get_width()),)
+                """if pair % 2 == 0:
+                    s = pygame.Surface((self.surface.get_height(),
+                                        self.surface.get_width()),)
+                    s.fill(Amazing.Const.WHITE)
+
+voir si c'est utile plus tard
 
                     print(f"pair{(i, j)}")
                 else:
-                    pygame.draw.rect(self.surface, Amazing.Const.BLACK, (i, j, 40, 40))
-                    print(f"impair {(i, j)}")
-                pair += 1
-            pair -= 1
+                    s = pygame.Surface((self.surface.get_height(),
+                                        self.surface.get_width()), )
+                    s.fill(Amazing.Const.BLACK)
+                    print(f"impair {(i, j)}")"""
+                self.cells.update({(ci, cj): {}})
+                test.append((ci, cj))
+                cj += 1
+            ci += 1
+        print(self.cells)
+        print(test)
