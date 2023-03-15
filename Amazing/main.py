4ad65d5a3daddb09c5b74da4176863fd0037d6ec
Hugo Lherm <hugolher@etudiant.univ-reims.fr>
2023-03-15 19:27:26 +0100

    Ajout des déplacements du joueur

--- Amazing/main.py
+++ Amazing/main.py
@@ -38,11 +38,72 @@ surface.blit(bords, (36, 36))
 
 jeu = laby.surface.game_surface
 jeu.fill(Const.BLACK)
-surface.blit(laby.afficher(jeu),(40, 40))
+maze = laby.afficher(jeu)
+surface.blit(maze,(40, 40))
 
 police = pygame.font.Font("font/8-bit-pusab.ttf", 16)
 text_surface = police.render("AMAZING", True, Const.WHITE)
 surface.blit(text_surface, (175, 5))
+
+image = pygame.image.load("images/the-rock-sus.png")
+image = pygame.transform.scale(image, (40,40))
+surface.blit(image, (40, 40))
+
+x = 40
+y = 40
+xc = 0
+yc = 0
+pos = (x, y)
+pos = (x, y)
+posc = (xc, yc)
+while pos != (400, 400):
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_RIGHT:
+                if (xc+1, yc) in laby.laby.get_reachable_cells(posc):
+                    x += 40
+                    xc += 1
+                    pos = (x, y)
+                    posc = (xc, yc)
+                    print(pos, posc)
+                    surface.blit(maze, (40, 40))
+                    surface.blit(image, pos)
+
+            if event.key == pygame.K_LEFT:
+                if (xc-1, yc) in laby.laby.get_reachable_cells(posc):
+                    x -= 40
+                    xc -= 1
+                    pos = (x, y)
+                    posc = (xc, yc)
+                    print(pos, posc)
+                    surface.blit(maze, (40, 40))
+                    surface.blit(image, pos)
+
+            if event.key == pygame.K_DOWN:
+                if (xc, yc+1) in laby.laby.get_reachable_cells(posc):
+                    y += 40
+                    yc += 1
+                    pos = (x, y)
+                    posc = (xc, yc)
+                    print(pos, posc)
+                    surface.blit(maze, (40, 40))
+                    surface.blit(image, pos)
+
+
+            if event.key == pygame.K_UP:
+                if (xc, yc-1) in laby.laby.get_reachable_cells(posc):
+                    y -= 40
+                    yc -= 1
+                    pos = (x, y)
+                    posc = (xc, yc)
+                    print(pos, posc)
+                    surface.blit(maze, (40, 40))
+                    surface.blit(image, pos)
+
+
 
 
 # Boucle de jeu
---
+++ Amazing/player/player.py
@@ -0,0 +1,10 @@
+import pygame
+from Amazing.map.maze import Maze
+from Amazing.map.GameZone import Zone
+class Player:
+
+    def __init__(self, image):
+
+
+        self.image = image
+        self.size = ()
